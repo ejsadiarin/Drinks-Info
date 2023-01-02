36519ffc6eba5bd16f94bbbda69aa2b3ef77f0f0
Ej Sadiarin <ejsadiarin@gmail.com>
2023-01-03 02:20:39 +0800

    API key

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+// Http client instance
+HttpClient client = new HttpClient();
+
+// set API key in request headers
+client.DefaultRequestHeaders.Add("API-key", "1");
+
+
