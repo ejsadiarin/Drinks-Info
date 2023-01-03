1e34c31a02b4b19e028f063b21adf7df923e3cd6
Ej Sadiarin <ejsadiarin@gmail.com>
2023-01-03 23:34:53 +0800

    added logic

--- Program.cs
+++ Program.cs
@@ -5,9 +5,25 @@ using System.Net.Http.Headers;
 using System.Text.Json;
 
 // Http client instance
-HttpClient client = new HttpClient();
+using HttpClient client = new HttpClient();
 
 // set API key in request headers
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 client.DefaultRequestHeaders.Add("API-key", "1");
+
+var repositories = await ProcessRepositoriesAsync(client);
+
+foreach (var repo in repositories)
+{
+    Console.WriteLine($"Name: {repo.Name}");
+}
+
+static async Task<List<Repository>> ProcessRepositoriesAsync(HttpClient client)
+{
+    await using Stream stream = await client.GetStreamAsync("www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail");
+    var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(stream);
+    return repositories ?? new();
+}
 
 
