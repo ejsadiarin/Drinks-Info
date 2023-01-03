03ec23305fff01f3faf40a798d3dc2493814250a
Ej Sadiarin <ejsadiarin@gmail.com>
2023-01-04 00:07:24 +0800

    TODO: resolve invalid request URI

--- Program.cs
+++ Program.cs
@@ -7,6 +7,14 @@ using System.Text.Json;
 // Http client instance
 HttpClient client = new();
 
+// GET method from API endpoint
+var response = await client.GetAsync("www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail");
 
+if (response.IsSuccessStatusCode)
+{
+    string responseContent = await response.Content.ReadAsStringAsync();
+    Console.WriteLine("Content response from API:" + responseContent + "\n");
+    Console.ReadLine();
+}
 
 
