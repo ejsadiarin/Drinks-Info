5c2a7f24de9f1f462116676d381f07efadb2cca0
Ej Sadiarin <ejsadiarin@gmail.com>
2023-01-04 00:00:51 +0800

    refactored new code now

--- Program.cs
+++ Program.cs
@@ -5,25 +5,8 @@ using System.Net.Http.Headers;
 using System.Text.Json;
 
 // Http client instance
-using HttpClient client = new HttpClient();
+HttpClient client = new();
 
-// set API key in request headers
-client.DefaultRequestHeaders.Accept.Clear();
-client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-client.DefaultRequestHeaders.Add("API-key", "1");
 
-var repositories = await ProcessRepositoriesAsync(client);
-
-foreach (var repo in repositories)
-{
-    Console.WriteLine($"Name: {repo.Name}");
-}
-
-static async Task<List<Repository>> ProcessRepositoriesAsync(HttpClient client)
-{
-    await using Stream stream = await client.GetStreamAsync("www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail");
-    var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(stream);
-    return repositories ?? new();
-}
 
 
